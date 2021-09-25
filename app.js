b7de05b60c4188ba962c4f4999938b143573035c
Justin Horein <justinhorein@gmail.com>
2021-09-25 11:03:49 -0700

    add custom css + search form

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+const path = require('path');
 const express = require("express"),
                 app = express();
 
+
+
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
     res.render("index");
